a5713dbbae84b110bd918da3555fd9c8aaa22208
Sergio Núñez <sergioop722@gmail.com>
2025-03-11 07:18:14 -0500

    se agrega colors para los mensajes de la consola

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import colors from 'colors'
 import app from "./server";
 
+const port = process.env.PORT || 4000
 app.listen(4000, () => {
-    console.log(`REST API en el puerto 4000`)
+    console.log(colors.yellow(`REST API en el puerto ${port}`))
 })
