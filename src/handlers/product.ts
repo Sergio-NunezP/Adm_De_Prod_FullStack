995c5073001ac200723cd4b61fc5cf57d3419e19
Sergio Núñez <sergioop722@gmail.com>
2025-03-11 21:38:17 -0500

    añadiendo el middleware

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,19 +1,16 @@
 import { Request, Response } from 'express'
-import { check, validationResult } from 'express-validator'
 import Product from '../models/Product.model'
 
 export const createProducts = async (req: Request, res: Response) => {
 
-    // Mostrat datos
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+
+    try {
+        // Almacenar en la base de datos
+        const product = await Product.create(req.body)
+        res.json({
+            data: product
+        })
+    } catch (error) {
+        console.log(error)
     }
-
-
-    // Almacenar en la base de datos
-    const product = await Product.create(req.body)
-    res.json({
-        data: product
-    })
 }
