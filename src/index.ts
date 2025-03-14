8892be3dfbd68e6a4144d3ac865bf5cbb63d38b8
Sergio Núñez <sergioop722@gmail.com>
2025-03-14 07:40:06 -0500

    probando la URL de productos

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors'
 import app from "./server";
 
-const port = process.env.PORT || 4001
-app.listen(4001, () => {
+const port = process.env.PORT || 4002
+app.listen(port, () => {
     console.log(colors.yellow(`REST API en el puerto ${port}`))
 })
