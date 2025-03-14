6d8610d7a7b5dcb8ebe532b3b7032fc19bd31493
Sergio Núñez <sergioop722@gmail.com>
2025-03-14 13:55:15 -0500

    trabajando en las métricas de validación

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,49 +3,36 @@ import Product from '../models/Product.model'
 
 //Obtener producto
 export const getProducts = async (req: Request, res: Response) => {
-    try {
-        const products = await Product.findAll({
-            order: [
-                ['price', 'ASC']
-            ],
-            attributes: { exclude: ['createdAt', 'updatedAt', 'availability'] }
-        })
-        res.json({ data: products })
-    } catch (error) {
-        console.log(error)
-    }
+    const products = await Product.findAll({
+        order: [
+            ['price', 'ASC']
+        ],
+        attributes: { exclude: ['createdAt', 'updatedAt', 'availability'] }
+    })
+    res.json({ data: products })
 }
 
 //Obtener producto por id
 export const getProductsById = async (req: Request, res: Response) => {
-    try {
-        // Obtener registro
-        const { id } = req.params
-        const product = await Product.findByPk(id)
+    // Obtener registro
+    const { id } = req.params
+    const product = await Product.findByPk(id)
 
-        if (!product) {
-            return res.status(404).json({
-                error: 'Producto No Encontrado'
-            })
-        }
-
-        res.json({ data: product })
-    } catch (error) {
-        console.log(error)
+    if (!product) {
+        return res.status(404).json({
+            error: 'Producto No Encontrado'
+        })
     }
+
+    res.json({ data: product })
 }
 
 
 // Crear producto
 export const createProducts = async (req: Request, res: Response) => {
-
-    try {
-        // Almacenar en la base de datos
-        const product = await Product.create(req.body)
-        res.status(201).json({ data: product })
-    } catch (error) {
-        console.log(error)
-    }
+    // Almacenar en la base de datos
+    const product = await Product.create(req.body)
+    res.status(201).json({ data: product })
 }
 
 // Actualizar producto
